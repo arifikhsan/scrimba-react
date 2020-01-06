5edd2a9318e2b9530035bde528b2d85bb355818f
Arif Ikhsanudin <arif.ikhsanudin.id@gmail.com>
2020-01-06 20:44:44 +0700

    unread messages

--- src/App.js
+++ src/App.js
@@ -1,8 +1,21 @@
 import React, { Component } from 'react';
 
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      unreadMessages: [
+        'Call your mom!',
+        'New spam email available. All links are definitely safe to click'
+      ]
+    };
+  }
   render() {
-    return <div>Hello</div>;
+    return (
+      <div>
+        <h2>You have {this.state.unreadMessages.length} unread messages!</h2>
+      </div>
+    );
   }
 }
 
