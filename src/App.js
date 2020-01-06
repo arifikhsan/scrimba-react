4081c505d64557ea5c98af63b8f696abc3e82008
Arif Ikhsanudin <arif.ikhsanudin.id@gmail.com>
2020-01-06 14:56:41 +0700

    counter using react state

--- src/App.js
+++ src/App.js
@@ -1,11 +1,29 @@
-import React from 'react';
+import React, { Component } from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      Hello 
-    </div>
-  );
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      count: 0
+    };
+    this.handleClick = this.handleClick.bind(this)
+  }
+  handleClick() {
+    this.setState(prevState => {
+      return {
+        count: prevState.count + 1
+      };
+    });
+  }
+
+  render() {
+    return (
+      <div className='App'>
+        {this.state.count}
+        <button onClick={this.handleClick}>Change!</button>
+      </div>
+    );
+  }
 }
 
 export default App;
