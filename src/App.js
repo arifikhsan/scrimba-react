b1b95e2a8090e3d350c3390e902b897f75838a95
Arif Ikhsanudin <arif.ikhsanudin.id@gmail.com>
2020-01-06 20:54:57 +0700

    practice done

--- src/App.js
+++ src/App.js
@@ -1,8 +1,30 @@
 import React, { Component } from 'react';
 
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      isLoggedIn: false
+    };
+
+    this.handleClick = this.handleClick.bind(this);
+  }
+
+  handleClick() {
+    this.setState({
+      isLoggedIn: !this.state.isLoggedIn
+    });
+  }
+
   render() {
-    return <div>Hello</div>;
+    return (
+      <div>
+        <h1>{this.state.isLoggedIn ? 'Logged In' : 'Logged Out'}</h1>{' '}
+        <button onClick={this.handleClick}>
+          {this.state.isLoggedIn ? 'Let me out' : 'Let me in'}
+        </button>
+      </div>
+    );
   }
 }
 
