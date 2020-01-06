4c070cb3ad772d45e260ca99f81962cde96dadbe
Arif Ikhsanudin <arif.ikhsanudin.id@gmail.com>
2020-01-06 22:15:51 +0700

    fix bug changing an uncontrolled input

--- src/App.js
+++ src/App.js
@@ -4,13 +4,15 @@ class App extends Component {
   constructor() {
     super();
     this.state = {
-      firstname: ''
+      firstname: '',
+      lastname: ''
     };
     this.handleChange = this.handleChange.bind(this);
   }
 
   handleChange(event) {
-    this.setState({ [event.target.name]: event.target.value });
+    const { name, value } = event.target;
+    this.setState({ [name]: value });
   }
 
   render() {
@@ -21,6 +23,7 @@ class App extends Component {
             type='text'
             placeholder='firstname'
             name='firstname'
+            value={this.state.firstname}
             onChange={this.handleChange}
           />
           <br />
@@ -28,6 +31,7 @@ class App extends Component {
             type='text'
             placeholder='lastname'
             name='lastname'
+            value={this.state.lastname}
             onChange={this.handleChange}
           />
           <br />
