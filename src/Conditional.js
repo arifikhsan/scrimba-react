0d648a81cce09d3d230820bc4b455ee20ac60cbf
Arif Ikhsanudin <arif.ikhsanudin.id@gmail.com>
2020-01-06 20:38:31 +0700

    aa

--- src/Conditional.js
+++ src/Conditional.js
@@ -1,8 +1,10 @@
 import React from 'react';
 
-function Conditional(props) {
+function Conditional() {
   return (
-    <div>{props.loading ? <h1>loading...</h1> : <h1>Cool stuff</h1>}</div>
+    <div>
+      <h1>Cool stuff</h1>
+    </div>
   );
 }
 
