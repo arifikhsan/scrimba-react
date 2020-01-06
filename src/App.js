d9762e9140058e91c92ac9a0abb0e204cf87abc8
Arif Ikhsanudin <arif.ikhsanudin.id@gmail.com>
2020-01-06 21:53:34 +0700

    fetch data and then show name

--- src/App.js
+++ src/App.js
@@ -1,8 +1,23 @@
 import React, { Component } from 'react';
 
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      character: {}
+    };
+  }
+  componentDidMount() {
+    fetch('https://swapi.co/api/people/1/?format=json')
+      .then(response => response.json())
+      .then(data => {
+        this.setState({
+          character: data
+        });
+      });
+  }
   render() {
-    return <div>Hello</div>;
+    return <div>{this.state.character.name}</div>;
   }
 }
 
