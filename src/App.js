6bb54a1fcbfc5dd12d41febca6453a7d52cdbc65
Arif Ikhsanudin <arif.ikhsanudin.id@gmail.com>
2020-01-06 22:07:46 +0700

    firstname and lastname

--- src/App.js
+++ src/App.js
@@ -3,11 +3,40 @@ import React, { Component } from 'react';
 class App extends Component {
   constructor() {
     super();
-    this.state = {};
+    this.state = {
+      firstname: ''
+    };
+    this.handleChange = this.handleChange.bind(this);
+  }
+
+  handleChange(event) {
+    this.setState({ [event.target.name]: event.target.value });
   }
 
   render() {
-    return <div>Code goes here</div>;
+    return (
+      <div>
+        <form>
+          <input
+            type='text'
+            placeholder='firstname'
+            name='firstname'
+            onChange={this.handleChange}
+          />
+          <br />
+          <input
+            type='text'
+            placeholder='lastname'
+            name='lastname'
+            onChange={this.handleChange}
+          />
+          <br />
+          <h1>
+            {this.state.firstname} {this.state.lastname}
+          </h1>
+        </form>
+      </div>
+    );
   }
 }
 
