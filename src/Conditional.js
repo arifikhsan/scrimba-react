e2ec6c25cb0e79872ebe8f69b672f897a640a9da
Arif Ikhsanudin <arif.ikhsanudin.id@gmail.com>
2020-01-06 20:33:41 +0700

    ternary

--- src/Conditional.js
+++ src/Conditional.js
@@ -1,11 +1,9 @@
 import React from 'react';
 
 function Conditional(props) {
-  if (props.loading) {
-    return <h1>loading...</h1>;
-  } else {
-    return <h1>Some cool stuff about conditional rendering</h1>;
-  }
+  return (
+    <div>{props.loading ? <h1>loading...</h1> : <h1>Cool stuff</h1>}</div>
+  );
 }
 
 export default Conditional;
