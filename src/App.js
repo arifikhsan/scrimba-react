5df6cc75e3557d179987888a0175d6527e90d1e7
Arif Ikhsanudin <arif.ikhsanudin.id@gmail.com>
2020-01-06 22:53:01 +0700

    add radio button

--- src/App.js
+++ src/App.js
@@ -6,7 +6,8 @@ class App extends Component {
     this.state = {
       firstname: '',
       lastname: '',
-      isFriendly: true
+      isFriendly: true,
+      gender: ''
     };
     this.handleChange = this.handleChange.bind(this);
   }
@@ -47,10 +48,35 @@ class App extends Component {
             />
             Is friendly?
           </label>
+          <br />
+
+          <label>
+            <input
+              type='radio'
+              name='gender'
+              value='male'
+              checked={this.state.gender === 'male'}
+              onChange={this.handleChange}
+            />
+            Male
+          </label>
+          <br />
+          <label>
+            <input
+              type='radio'
+              name='gender'
+              value='female'
+              checked={this.state.gender === 'female'}
+              onChange={this.handleChange}
+            />
+            Female
+          </label>
+          <br />
 
           <h1>
             {this.state.firstname} {this.state.lastname}
           </h1>
+          <h2>You are a {this.state.gender}</h2>
         </form>
       </div>
     );
