84d6ee89fbfe3bb1569770be185d3f925ff23a0e
Arif Ikhsanudin <arif.ikhsanudin.id@gmail.com>
2020-01-07 14:23:50 +0700

    without binding function

--- src/App.js
+++ src/App.js
@@ -3,11 +3,29 @@ import React, { Component } from 'react';
 class App extends Component {
   constructor() {
     super();
-    this.state = {};
+    this.state = {
+      name: ''
+    };
   }
 
+  handleChange = event => {
+    const { name, value } = event.target;
+    this.setState({ [name]: value });
+  };
+
   render() {
-    return <div>Hello</div>;
+    return (
+      <div>
+        <input
+          type='text'
+          name='name'
+          value={this.state.name}
+          onChange={this.handleChange}
+        />
+        <br/>
+        {this.state.name}
+      </div>
+    );
   }
 }
 
